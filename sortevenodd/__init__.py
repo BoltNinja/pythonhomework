bccb78f0f16f589be8df99d44549c5e820f9ae13
Aarav-Ansh <aaravgupta090@gmail.com>
2019-09-28 14:11:27 -0400

    i am grooti am inevitable

---
+++ sortevenodd/__init__.py
@@ -0,0 +1,5 @@
+modulus = int(input("Type a number right now!!!!!"))
+if modulus%2 == 1:
+    print("This is an odd number.")
+else:
+    print("This an even number.")
---
+++ variables.py
@@ -0,0 +1,14 @@
+#you have to type stuff in for inputs
+first = int(input("Enter number 1"))
+last = int(input("Enter number 2"))
+print(first + " " + last)
+
+
+#Modulus
+#Modulus gives you a remainder
+print (a%b)
+
+
+
+
+
